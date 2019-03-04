4c399ff207a8e874afa11454eed685959db81a34
benoit-bernier <benoit.bernier@etudiant.univ-rennes1.fr>
2019-03-04 15:58:12 +0100

    connection, allumage 2,4, potentiel chenillard

--- knx_nodejs/index.js
+++ knx_nodejs/index.js
@@ -4,18 +4,21 @@ let connection = new knx.Connection({
   // ip address and port of the KNX router or interface
   ipAddr: "192.168.0.5",
   port: 3671,
-  minimumDelay: 1000,
+  minimumDelay: 50,
   handlers: {
     // wait for connection establishment before sending anything!
     connected: function() {
       console.log("Connected !");
       // Allumer la LED
-      connection.write("0/0/1", 1);
+      connection.write("0/1/1", 0);
       sleep();
-      connection.write("0/0/1", 1);
+      connection.write("0/1/2", 1);
       sleep();
-      connection.write("0/0/1", 1);
+      connection.write("0/1/3", 0);
       sleep();
+      connection.write("0/1/4",1);
+      sleep();
+      //chenillard();
       // you can also issue a READ request and pass a callback to capture the response
       connection.read("0/2/1", (src, responsevalue) => {
         console.log("src : " + src + ", responsevalue" + responsevalue);
@@ -43,3 +46,29 @@ let connection = new knx.Connection({
     }
   }
 });
+
+function chenillard(){
+  while(true){
+    connection.write("0/1/1", 1);
+    connection.write("0/1/2", 0);
+    connection.write("0/1/3", 0);
+    connection.write("0/1/4", 0);
+    sleep();
+    connection.write("0/1/1", 0);
+    connection.write("0/1/2", 1);
+    connection.write("0/1/3", 0);
+    connection.write("0/1/4", 0);
+    sleep();
+    connection.write("0/1/1", 0);
+    connection.write("0/1/2", 0);
+    connection.write("0/1/3", 1);
+    connection.write("0/1/4", 0);
+    sleep();
+    connection.write("0/1/1", 0);
+    connection.write("0/1/2", 0);
+    connection.write("0/1/3", 0);
+    connection.write("0/1/4", 1);
+    sleep();
+
+  }
+}
