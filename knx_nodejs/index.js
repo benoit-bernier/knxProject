cbf0ff6a3483acba240cb2a3d8d85f76bce49e16
blandela <blandel.alex@gmail.com>
2019-03-04 14:33:20 +0100

    V0

--- knx_nodejs/index.js
+++ knx_nodejs/index.js
@@ -1,1 +1,21 @@
-console.log("Hello world !");
+var knx = require("knx");
+var connection = knx.Connection({
+  handlers: {
+    connected: function() {
+      console.log("Connected!");
+    },
+    event: function(evt, src, dest, value) {
+      console.log(
+        "%s **** KNX EVENT: %j, src: %j, dest: %j, value: %j",
+        new Date()
+          .toISOString()
+          .replace(/T/, " ")
+          .replace(/\..+/, ""),
+        evt,
+        src,
+        dest,
+        value
+      );
+    }
+  }
+});
