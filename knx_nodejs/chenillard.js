e6cb2add42b69581a17c016040f72b897985eeec
blandela <blandel.alex@gmail.com>
2019-03-18 13:30:59 +0100

    Ajout action pour éteindre les LEDS

--- knx_nodejs/chenillard.js
+++ knx_nodejs/chenillard.js
@@ -1,6 +1,6 @@
 let state = 0;
 let speed = 1000;
-let speed_ratio = 1;
+let speed_ratio = 1.5;
 
 let mChenillard = setInterval(function() {
   chenillard(state);
@@ -12,15 +12,33 @@ function chenillard(state) {
   switch (state) {
     case 0:
       console.log("Lancer LED 1");
+      setTimeout(function() {
+        console.log("Eteindre LED 0");
+        console.log("======");
+      }, (speed * speed_ratio) / 2);
       break;
     case 1:
       console.log("Lancer LED 2");
+      setTimeout(function() {
+        console.log("Eteindre LED 1");
+        console.log("======");
+      }, (speed * speed_ratio) / 2);
       break;
     case 2:
       console.log("Lancer LED 3");
+      setTimeout(function() {
+        console.log("Eteindre LED 2");
+        console.log("======");
+      }, (speed * speed_ratio) / 2);
+
       break;
     case 3:
       console.log("Lancer LED 0");
+      setTimeout(function() {
+        console.log("Eteindre LED 3");
+        console.log("======");
+      }, (speed * speed_ratio) / 2);
+
       break;
     default:
       console.log("Stop");
@@ -35,7 +53,7 @@ setTimeout(function() {
 
 setTimeout(function() {
   //state = 0;
-  speed_ratio = 0.5;
+  speed_ratio = 2;
   clearInterval(mChenillard);
   mChenillard = setInterval(function() {
     chenillard(state);
@@ -46,7 +64,7 @@ setTimeout(function() {
 
 setTimeout(function() {
   //state = 0;
-  speed_ratio = 0.25;
+  speed_ratio = 2.5;
   clearInterval(mChenillard);
   mChenillard = setInterval(function() {
     chenillard(state);
