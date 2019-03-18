f7b41caa8997de2f48c06c3cd891b7c552deea4f
benoit-bernier <benoit.bernier@etudiant.univ-rennes1.fr>
2019-03-18 14:12:11 +0100

    modif adresses leds

--- knx_nodejs/index.js
+++ knx_nodejs/index.js
@@ -7,78 +7,86 @@ let connection = new knx.Connection({
   minimumDelay: 50,
   handlers: {
     // wait for connection establishment before sending anything!
-    connected: function() {
+    connected: function () {
       // partie connexion
       let state = 0; // state for the chenillar
       let speed = 1000; // default time between two commands
-      let speed_ratio = 1; // real speed = speed * speed_ratio (allow to increase or decrease the speed of the chenillar)
+      let speed_ratio = 1.5; // real speed = speed * speed_ratio (allow to increase or decrease the speed of the chenillar)
 
       console.log("Connected !");
 
       //lancement du chenillar
       //allume une LED avec l'état correspondant
-      let mChenillar = setInterval(function() {
-        chenillar(state);
+
+      let mChenillard = setInterval(function () {
+        chenillard(state);
         state = (state + 1) % 4;
-      }, 2000);
+      }, speed * speed_ratio);
 
-      //coupe le chenillar après 20 secondes
-      setTimeout(function() {
-        clearInterval(mChenillar);
-      }, 20000);
 
-      //augmente la vitesse du chenillar après 10 secondes
-      setTimeout(function() {
-        state = 0;
-        speed_ratio = 0.5;
-        clearInterval(mChenillar);
-        mChenillar = setInterval(function() {
-          chenillar(state);
-          state = (state + 1) % 4;
-          console.log(speed_ratio);
-        }, speed * speed_ratio);
-      }, 10000);
 
       //permet d'allumer les LED les unes après les autres
       function chenillar(state) {
         switch (state) {
           case 0:
             console.log("Lancé LED 1");
-            connection.write("0/0/2", 1);
+            connection.write("0/1/1", 1);
             //ou coupe la LED précédente après 1 seconde
-            setTimeout(function() {
-              connection.write("0/0/1", 0);
-            }, speed * speed_ratio);
+            setTimeout(function () {
+              connection.write("0/1/4", 0);
+            }, (speed * speed_ratio) / 2);
             break;
           case 1:
             console.log("Lancé LED 2");
-            connection.write("0/0/3", 1);
-            setTimeout(function() {
-              connection.write("0/0/2", 0);
-            }, speed * speed_ratio);
+            connection.write("0/1/2", 1);
+            setTimeout(function () {
+              connection.write("0/1/1", 0);
+            }, (speed * speed_ratio) / 2);
             break;
           case 2:
             console.log("Lancé LED 3");
-            connection.write("0/0/4", 1);
-            setTimeout(function() {
-              connection.write("0/0/3", 0);
-            }, speed * speed_ratio);
+            connection.write("0/1/3", 1);
+            setTimeout(function () {
+              connection.write("0/1/2", 0);
+            }, (speed * speed_ratio) / 2);
             break;
           case 3:
             console.log("Lancé LED 0");
-            connection.write("0/0/1", 1);
-            setTimeout(function() {
-              connection.write("0/0/4", 0);
-            }, speed * speed_ratio);
+            connection.write("0/1/4", 1);
+            setTimeout(function () {
+              connection.write("0/1/3", 0);
+            }, (speed * speed_ratio) / 2);
             break;
           default:
-            console.log("STOP chenillar");
+            console.log("STOP chenillard");
         }
       }
+      setTimeout(function () {
+        //state = 0;
+        speed_ratio = 2;
+        clearInterval(mChenillard);
+        mChenillard = setInterval(function () {
+          chenillard(state);
+          state = (state + 1) % 4;
+          console.log(speed_ratio);
+        }, speed * speed_ratio);
+      }, 10000);
+
+      setTimeout(function () {
+        //state = 0;
+        speed_ratio = 2.5;
+        clearInterval(mChenillard);
+        mChenillard = setInterval(function () {
+          chenillard(state);
+          state = (state + 1) % 4;
+          console.log(speed_ratio);
+        }, speed * speed_ratio);
+      }, 20000);
+
       // fin partie connexion
     },
     // get notified for all KNX events:
-    event: function(evt, src, dest, value) {
+    event: function (evt, src, dest, value) {
       console.log(
         "event: %s, src: %j, dest: %j, value: %j",
         evt,
@@ -88,14 +96,14 @@ let connection = new knx.Connection({
       );
     },
     // get notified on connection errors
-    error: function(connstatus) {
+    error: function (connstatus) {
       console.log("**** ERROR: %j", connstatus);
     }
   }
 });
 
-function chenillard(){
-  while(true){
+function chenillard() {
+  while (true) {
     connection.write("0/1/1", 1);
     connection.write("0/1/2", 0);
     connection.write("0/1/3", 0);
