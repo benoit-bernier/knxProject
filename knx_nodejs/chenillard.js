9efa772f91b4f2fea7e0f3a3af35d2724aa0ba5a
benoit-bernier <benoit.bernier@etudiant.univ-rennes1.fr>
2019-03-19 10:04:56 +0100

    création classe

--- knx_nodejs/chenillard.js
+++ knx_nodejs/chenillard.js
@@ -1,81 +1,32 @@
-let state = 0;
-let speed = 1000;
-let speed_ratio = 1.5;
+class chenillard {
+  constructor(tableau, intervalle, state) {
+    this.tableau = tableau;
+    this.intervalle = intervalle;
+    this.state = state;
+  }
+  get tableau() {
+    return this.tableau;
+  }
+  get state() {
+    return this.state;
+  }
+  get intervalle() {
+    return this.intervalle;
+  }
+  set tableau(tab) {
+    this.tableau = tab;
+  }
+  set state(state) {
+    this.state = state;
+  }
+  set intervalle(intervalle) {
+    intervalle;
+  }
 
-console.log((0 * -1 + 4) % 4);
-console.log((1 * -1 + 4) % 4);
-console.log((2 * -1 + 4) % 4);
-console.log((3 * -1 + 4) % 4);
-console.log((4 * -1 + 4) % 4);
-console.log(((5 * -1) % 4) + 4);
-
-let mChenillard = setInterval(function() {
-  chenillard(state);
-  state = (state + 1) % 4;
-}, speed * speed_ratio);
-
-function chenillard(state) {
-  console.log(state);
-  switch (state) {
-    case 0:
-      console.log("Lancer LED 1");
-      setTimeout(function() {
-        console.log("Eteindre LED 0");
-        console.log("======");
-      }, (speed * speed_ratio) / 2);
-      break;
-    case 1:
-      console.log("Lancer LED 2");
-      setTimeout(function() {
-        console.log("Eteindre LED 1");
-        console.log("======");
-      }, (speed * speed_ratio) / 2);
-      break;
-    case 2:
-      console.log("Lancer LED 3");
-      setTimeout(function() {
-        console.log("Eteindre LED 2");
-        console.log("======");
-      }, (speed * speed_ratio) / 2);
-
-      break;
-    case 3:
-      console.log("Lancer LED 0");
-      setTimeout(function() {
-        console.log("Eteindre LED 3");
-        console.log("======");
-      }, (speed * speed_ratio) / 2);
-
-      break;
-    default:
-      console.log("Stop");
+  stop() {
+    console.log("chenillard arrêté");
+  }
+  start() {
+    console.log("chenillard démarré");
   }
 }
-
-/*
-setTimeout(function() {
-  clearInterval(mChenillard);
-}, 30000);
-*/
-
-setTimeout(function() {
-  //state = 0;
-  speed_ratio = 2;
-  clearInterval(mChenillard);
-  mChenillard = setInterval(function() {
-    chenillard(state);
-    state = (state + 1) % 4;
-    console.log(speed_ratio);
-  }, speed * speed_ratio);
-}, 10000);
-
-setTimeout(function() {
-  //state = 0;
-  speed_ratio = 2.5;
-  clearInterval(mChenillard);
-  mChenillard = setInterval(function() {
-    chenillard(state);
-    state = (state + 1) % 4;
-    console.log(speed_ratio);
-  }, speed * speed_ratio);
-}, 20000);
