a492a83b8eb3f292c9450a27dc6f2db66a0a7f66
benoit-bernier <benoit.bernier@etudiant.univ-rennes1.fr>
2019-03-19 10:22:46 +0100

    patch conflict

--- knx_nodejs/chenillard.js
+++ knx_nodejs/chenillard.js
@@ -1,45 +1,3 @@
-<<<<<<< HEAD
-let state = 0;
-let speed = 1000;
-let speed_ratio = 1.5;
-
-let mChenillard = setInterval(function() {
-  chenillard(state);
-  state = (state + 1) % 4;
-}, speed * speed_ratio);
-
-function chenillard(state) {
-  console.log(state);
-  switch (state) {
-    case 0:
-      console.log("Lancer LED 1");
-      setTimeout(function() {
-        console.log("Eteindre LED 0");
-        console.log("======");
-      }, (speed * speed_ratio) / 2);
-      break;
-    case 1:
-      console.log("Lancer LED 2");
-      setTimeout(function() {
-        console.log("Eteindre LED 1");
-        console.log("======");
-      }, (speed * speed_ratio) / 2);
-      break;
-    case 2:
-      console.log("Lancer LED 3");
-      setTimeout(function() {
-        console.log("Eteindre LED 2");
-        console.log("======");
-      }, (speed * speed_ratio) / 2);
-
-      break;
-    case 3:
-      console.log("Lancer LED 0");
-      setTimeout(function() {
-        console.log("Eteindre LED 3");
-        console.log("======");
-      }, (speed * speed_ratio) / 2);
-=======
 class chenillard {
   constructor(tableau, intervalle, state) {
     this.tableau = tableau;
@@ -64,7 +22,6 @@ class chenillard {
   set intervalle(intervalle) {
     intervalle;
   }
->>>>>>> 9efa772f91b4f2fea7e0f3a3af35d2724aa0ba5a
 
   stop() {
     console.log("chenillard arrêté");
