bcf3f324c254bb161ff372f188e3ece87e1156ab
blandela <blandel.alex@gmail.com>
2019-03-20 09:45:41 +0100

    Réduction lignes de code

--- knx_nodejs/server.js
+++ knx_nodejs/server.js
@@ -19,12 +19,101 @@ let speed_ratio = 100; // real speed = speed + speed_ratio (allow to increase or
 let schema = [0, 1, 2, 3]; // schéma allumage des LED par défaut
 let mchenillard = ""; //instance du chenillard
 
+init();
+
 function init() {
   state = 0;
   speed = 500;
   speed_ratio = 100;
   schema = [0, 1, 2, 3];
   mchenillard = "";
+  connection = new knx.Connection({
+    // ip address and port of the KNX router or interface
+    ipAddr: ip_maquette,
+    port: port_maquette,
+    minimumDelay: 50,
+    handlers: {
+      // wait for connection establishment before sending anything!
+      connected: function() {
+        // partie connexion
+        console.log("Connected !");
+        connected = true;
+        // fin partie connexion
+      },
+      // get notified for all KNX events:
+      event: function(evt, src, dest, value) {
+        switch (dest) {
+          case "0/3/1":
+            if (speed_ratio === 0) {
+              console.log("Impossible d'accélérer.");
+            } else {
+              //Accelere
+              clearInterval(mchenillard);
+              mchenillard = setInterval(function() {
+                chenillard(state);
+                state = (state + 1) % 4;
+              }, speed + speed_ratio);
+              speed_ratio -= 100;
+              console.log("La vitesse est de :" + speed_ratio);
+            }
+            break;
+          case "0/3/2":
+            if (speed_ratio > 100000) {
+              console.log("Impossible de ralentir.");
+            } else {
+              //Ralenti
+              speed_ratio += 100;
+              clearInterval(mchenillard);
+              mchenillard = setInterval(function() {
+                chenillard(state);
+                state = (state + 1) % 4;
+              }, speed + speed_ratio);
+              console.log("La vitesse est de :" + speed_ratio);
+            }
+            //Ralenti
+            break;
+          case "0/3/3":
+            if (mchenillard == "") {
+              mchenillard = setInterval(function() {
+                chenillard(state);
+                state = (state + 1) % 4;
+                console.log("Lancement du chenillard");
+              }, speed + speed_ratio);
+            } else {
+              console.log("Stop du chenillard");
+              clearInterval(mchenillard);
+              mchenillard = "";
+            }
+            break;
+          case "0/3/4":
+            console.log("Changement de sens du chenillard");
+            schema.reverse();
+            console.log(schema);
+            clearInterval(mchenillard);
+            mchenillard = setInterval(function() {
+              chenillard(state);
+              state = (state + 1) % 4;
+            }, speed + speed_ratio);
+            break;
+
+          default:
+        }
+        /*
+        console.log(
+          "event: %s, src: %j, dest: %j, value: %j",
+          evt,
+          src,
+          dest,
+          value
+        );
+        */
+      },
+      // get notified on connection errors
+      error: function(connstatus) {
+        console.log("**** ERROR: %j", connstatus);
+      }
+    }
+  });
 }
 
 function chenillard(state) {
@@ -39,94 +128,6 @@ app.get("/", function(req, res) {
   res.sendFile(__dirname + "/html/index.html");
 });
 
-connection = new knx.Connection({
-  // ip address and port of the KNX router or interface
-  ipAddr: ip_maquette,
-  port: port_maquette,
-  minimumDelay: 50,
-  handlers: {
-    // wait for connection establishment before sending anything!
-    connected: function() {
-      // partie connexion
-      console.log("Connected !");
-      connected = true;
-      // fin partie connexion
-    },
-    // get notified for all KNX events:
-    event: function(evt, src, dest, value) {
-      switch (dest) {
-        case "0/3/1":
-          if (speed_ratio === 0) {
-            console.log("Impossible d'accélérer.");
-          } else {
-            //Accelere
-            clearInterval(mchenillard);
-            mchenillard = setInterval(function() {
-              chenillard(state);
-              state = (state + 1) % 4;
-            }, speed + speed_ratio);
-            speed_ratio -= 100;
-            console.log("La vitesse est de :" + speed_ratio);
-          }
-          break;
-        case "0/3/2":
-          if (speed_ratio > 100000) {
-            console.log("Impossible de ralentir.");
-          } else {
-            //Ralenti
-            speed_ratio += 100;
-            clearInterval(mchenillard);
-            mchenillard = setInterval(function() {
-              chenillard(state);
-              state = (state + 1) % 4;
-            }, speed + speed_ratio);
-            console.log("La vitesse est de :" + speed_ratio);
-          }
-          //Ralenti
-          break;
-        case "0/3/3":
-          if (mchenillard == "") {
-            mchenillard = setInterval(function() {
-              chenillard(state);
-              state = (state + 1) % 4;
-              console.log("Lancement du chenillard");
-            }, speed + speed_ratio);
-          } else {
-            console.log("Stop du chenillard");
-            clearInterval(mchenillard);
-            mchenillard = "";
-          }
-          break;
-        case "0/3/4":
-          console.log("Changement de sens du chenillard");
-          schema.reverse();
-          console.log(schema);
-          clearInterval(mchenillard);
-          mchenillard = setInterval(function() {
-            chenillard(state);
-            state = (state + 1) % 4;
-          }, speed + speed_ratio);
-          break;
-
-        default:
-      }
-      /*
-      console.log(
-        "event: %s, src: %j, dest: %j, value: %j",
-        evt,
-        src,
-        dest,
-        value
-      );
-      */
-    },
-    // get notified on connection errors
-    error: function(connstatus) {
-      console.log("**** ERROR: %j", connstatus);
-    }
-  }
-});
-
 io.on("connection", function(socket) {
   socket.on("events", function(data) {
     console.log(data);
@@ -135,92 +136,7 @@ io.on("connection", function(socket) {
     switch (input.cmd) {
       case "CONNECT":
         if (!connected) {
-          connection = new knx.Connection({
-            // ip address and port of the KNX router or interface
-            ipAddr: "192.168.0.5",
-            port: 3671,
-            minimumDelay: 50,
-            handlers: {
-              // wait for connection establishment before sending anything!
-              connection: function() {
-                // partie connexion
-                console.log("connection !");
-                connected = true;
-                // fin partie connexion
-              },
-              // get notified for all KNX events:
-              event: function(evt, src, dest, value) {
-                switch (dest) {
-                  case "0/3/1":
-                    if (speed_ratio === 0) {
-                      console.log("Impossible d'accélérer.");
-                    } else {
-                      //Accelere
-                      clearInterval(mchenillard);
-                      mchenillard = setInterval(function() {
-                        chenillard(state);
-                        state = (state + 1) % 4;
-                      }, speed + speed_ratio);
-                      speed_ratio -= 1;
-                      console.log("La vitesse est de :" + speed_ratio);
-                    }
-                    break;
-                  case "0/3/2":
-                    if (speed_ratio > 5000) {
-                      console.log("Impossible de ralentir.");
-                    } else {
-                      //Ralenti
-                      speed_ratio += 1;
-                      clearInterval(mchenillard);
-                      mchenillard = setInterval(function() {
-                        chenillard(state);
-                        state = (state + 1) % 4;
-                      }, speed + speed_ratio);
-                      console.log("La vitesse est de :" + speed_ratio);
-                    }
-                    //Ralenti
-                    break;
-                  case "0/3/3":
-                    if (mchenillard == "") {
-                      mchenillard = setInterval(function() {
-                        chenillard(state);
-                        state = (state + 1) % 4;
-                        console.log("Lancement du chenillard");
-                      }, speed + speed_ratio);
-                    } else {
-                      console.log("Stop du chenillard");
-                      clearInterval(mchenillard);
-                      mchenillard = "";
-                    }
-                    break;
-                  case "0/3/4":
-                    console.log("Changement de sens du chenillard");
-                    schema.reverse();
-                    clearInterval(mchenillard);
-                    mchenillard = setInterval(function() {
-                      chenillard(state);
-                      state = (state + 1) % 4;
-                    }, speed + speed_ratio);
-                    break;
-
-                  default:
-                }
-                /*
-            console.log(
-              "event: %s, src: %j, dest: %j, value: %j",
-              evt,
-              src,
-              dest,
-              value
-            );
-            */
-              },
-              // get notified on connection errors
-              error: function(connstatus) {
-                console.log("**** ERROR: %j", connstatus);
-              }
-            }
-          });
+          init();
         } else {
           console.log("Serveur déjà connecté à la maquette");
         }
