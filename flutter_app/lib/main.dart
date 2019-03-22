fbb6a31b4bf3627ead6c4f513d576754412c5c46
benoit-bernier <benoit.bernier@etudiant.univ-rennes1.fr>
2019-03-22 21:48:44 +0100

    Ajout Stateful icon

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,90 +1,104 @@
-// This sample shows adding an action to an [AppBar] that opens a shopping cart.
-
 import 'package:flutter/material.dart';
+// Uncomment lines 7 and 10 to view the visual layout at runtime.
+// import 'package:flutter/rendering.dart' show debugPaintSizeEnabled;
+import 'package:flutter/rendering.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  //debugPaintSizeEnabled = true;
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    Color color = Theme.of(context).primaryColor;
+
     return MaterialApp(
-      title: 'Flutter Code Sample for material.AppBar.actions',
-      theme: ThemeData(
-        primarySwatch: Colors.pink,
-      ),
-      home: MyStatelessWidget(),
-    );
+      title: 'Flutter layout demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Chenillard App"),
+          actions: <Widget>[
+            FavoriteWidget(),
+          ],
+        ),
+        body: Center(child:
+        Column(
+          children: <Widget>[
+            Text('Deliver features faster'),
+            ButtonBar(
+              alignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                RaisedButton(
+                  onPressed: () {},
+                  color: Colors.pink,
+                  child: Text('Accélère'),
+                  padding: EdgeInsets.only(left: 10.0, right: 10.0),
+                ),
+                RaisedButton(
+                    onPressed: () {},
+                    child: Text('Ralenti'),
+                    padding: EdgeInsets.only(left: 10.0, right: 10.0)
+                ),
+              ],
+            ),
+
+            Text('Craft beautiful UIs'),
+            ButtonBar(
+              alignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                RaisedButton(
+                  onPressed: () {},
+                  color: Colors.pink,
+                  child: Text('Accélère'),
+                  padding: EdgeInsets.only(left: 10.0, right: 10.0),
+                ),
+                RaisedButton(
+                    onPressed: () {},
+                    child: Text('Ralenti'),
+                    padding: EdgeInsets.only(left: 10.0, right: 10.0)
+                ),
+              ],
+            ),
+          ],
+        ),
+        ),
+        ),
+      );
   }
 }
 
-class MyStatelessWidget extends StatelessWidget {
-  MyStatelessWidget({Key key}) : super(key: key);
+class FavoriteWidget extends StatefulWidget{
+  @override
+  _FavoriteWidgetState createState() => _FavoriteWidgetState();
+}
 
-
+class _FavoriteWidgetState extends State<FavoriteWidget>{
+  bool _isConnected = true;
   @override
   Widget build(BuildContext context) {
-    /*
-    final showSnack = () => FlutterToast.showToast(
-      msg:"Bouton cliqué",
-      toastLength:Toast.LENGTH_SHORT
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Container(
+          padding: EdgeInsets.all(0),
+          child: IconButton(
+            icon: (_isConnected ? Icon(Icons.file_upload) : Icon(Icons.file_download)),
+            color: Colors.red[500],
+            onPressed: _toggleFavorite,
+          ),
+        ),
+      ],
     );
-    */
+  }
 
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Hello World'),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.file_download),
-            tooltip: 'Open shopping cart',
-            onPressed: () {
-              // ...
-            },
-          ),
-        ],
-      ),
-      body: Center(child:
-      Column(
-        children: <Widget>[
-          Text('Deliver features faster'),
-          ButtonBar(
-            alignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              RaisedButton(
-                onPressed: () {},
-                color: Colors.pink,
-                child: Text('Accélère'),
-                padding: EdgeInsets.only(left: 10.0, right: 10.0),
-              ),
-              RaisedButton(
-                  onPressed: () {},
-                  child: Text('Ralenti'),
-                  padding: EdgeInsets.only(left: 10.0, right: 10.0)
-              ),
-            ],
-          ),
-
-          Text('Craft beautiful UIs'),
-          ButtonBar(
-            alignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              RaisedButton(
-                onPressed: () {},
-                color: Colors.pink,
-                child: Text('Accélère'),
-                padding: EdgeInsets.only(left: 10.0, right: 10.0),
-              ),
-              RaisedButton(
-                  onPressed: () {},
-                  child: Text('Ralenti'),
-                  padding: EdgeInsets.only(left: 10.0, right: 10.0)
-              ),
-            ],
-          ),
-        ],
-      ),
-      ),
-    );
+  void _toggleFavorite() {
+    setState(() {
+      if (_isConnected) {
+        _isConnected = false;
+      } else {
+        _isConnected = true;
+      }
+    });
   }
 }
